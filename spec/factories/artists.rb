30f4c7b4a0a4cdd65fde24115a546a4f6582ac89
Kate <katepdonahue@gmail.com>
2014-03-28 13:25:40 -0400

    Add some factories for artists and artworks

---
+++ spec/factories/artists.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :artists do
+    name "Lucas Cranach the Elder"
+  end
+end
---
+++ spec/factories/artworks.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :artworks do
+    image_name "Portrait_of_a_Woman"
+    title "Portrait of a Woman"
+    year 1522
+    gallery "National Gallery of Art, Washington D.C."
+    artist_id 1
+  end
+end
