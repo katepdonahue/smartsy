21e5a249d4c855de57b21d345037d287876a1c81
Kate <katepdonahue@gmail.com>
2014-03-28 14:01:40 -0400

    Enter basic cucumber steps for display art and guess artist features

---
+++ features/step_definitions/display_art.rb
@@ -0,0 +1,35 @@
+Given(/^there are photos in the database$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I go to the game play page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the game play page should show the art images$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the game play page should show the images' titles$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the game play page should not show the artists' names$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am on the game play page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I enter a guess "(.*?)" for the artwork with id "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should get a point$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should not get a point$/) do
+  pending # express the regexp above with the code you wish you had
+end
---
+++ features/step_definitions/guess_artist.rb
@@ -0,0 +1,16 @@
+
+Given(/^I am on the game play page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I enter a guess "(.*?)" for the artwork with id "(.*?)"$/) do |artist, art_id|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should get a point$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should not get a point$/) do
+  pending # express the regexp above with the code you wish you had
+end
