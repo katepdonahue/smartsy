ab3c1aeeaa582765819fcde16022b775a497ab53
Kate <katepdonahue@gmail.com>
2014-03-28 14:28:03 -0400

    Write basic scraper test

--- spec/models/scraper_spec.rb
+++ spec/models/scraper_spec.rb
@@ -2,7 +2,13 @@ require 'spec-helper'
 
 describe Scraper do
 
-  describe "#"
+  describe "#grab_images" do
+    it "should grab all the photos and save them in the database" do
+      bauhaus_scraper = Scraper.new("bauhaus")
+      bauhaus_scraper.grab_images
+      expect(Artwork.find_by_name("Gasse").length).to eq(1)
+    end
+  end
 
 
 
