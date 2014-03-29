fc2bc53f9d50bed87f786683f6cdd82cc08cc620
Kate <katepdonahue@gmail.com>
2014-03-29 16:10:30 -0400

    Break down parse_data test

--- spec/models/scraper_spec.rb
+++ spec/models/scraper_spec.rb
@@ -18,10 +18,26 @@ describe Scraper do
   end
 
   describe "#parse_data" do
-    it "should save the correct data to an artist and artwork object" do
-      bauhaus_scraper = Scraper.new("bauhaus")
-      data = bauhaus_scraper.open_page("1")
-      expect(bauhaus_scraper.parse_data(data)).to eq(Artwork.find(1))
+    bauhaus_scraper = Scraper.new("bauhaus")
+    data = bauhaus_scraper.open_page("1")
+    artwork = bauhaus_scraper.parse_data(data)
+    it "should save the correct artist_id for the artwork object" do
+      expect(artwork.artist_id).to eq(1)
+    end
+    it "should save the correct title for the artwork object" do
+      expect(artwork.title).to eq("Study for Homage to the square: Absorption")
+    end
+    it "should save the correct year for the artwork object" do
+      expect(artwork.year).to eq(1967)
+    end
+    it "should save the correct gallery for the artwork object" do
+      expect(artwork.gallery).to eq("Vedovi Gallery")
+    end
+    it "should save the correct category for the artwork object" do
+      expect(artwork.category).to eq("bauhaus")
+    end
+    it "should save the correct image_id for the artwork object" do
+      expect(artwork.image_id).to eq("5331ae928b3b81a6b5000009")
     end
   end
 
