f84e22c9622b8926b3857162a8c2a958b828ee70
Kate <katepdonahue@gmail.com>
2014-03-29 16:28:58 -0400

    Write test to make sure artists don't get duplicated

--- spec/models/scraper_spec.rb
+++ spec/models/scraper_spec.rb
@@ -3,10 +3,13 @@ require 'spec_helper'
 describe Scraper do
 
   describe "#grab_pages" do
+    bauhaus_scraper = Scraper.new("bauhaus")
+    bauhaus_scraper.grab_pages
     it "should grab all the photos and save them in the database" do
-      bauhaus_scraper = Scraper.new("bauhaus")
-      bauhaus_scraper.grab_pages
       expect(Artwork.find_by(:title => "Study for Homage to the square: Absorption")).not_to be_nil
+    end
+    it "should not create duplicate artists in the database" do
+      expect(Artist.where(:name => "Josef Albers").length).to eq(1)
     end
   end
 
