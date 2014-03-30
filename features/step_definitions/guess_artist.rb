6b4a2d584f71230a99d3f1ff9fd33dccceb02a26
Kate <katepdonahue@gmail.com>
2014-03-30 13:08:45 -0400

    Prepare steps for guess artist feature

--- features/step_definitions/guess_artist.rb
+++ features/step_definitions/guess_artist.rb
@@ -1,16 +1,13 @@
 
-Given(/^I am on the game play page$/) do
-  visit game_path
+Given(/^I am on the "(.*?)" game play page$/) do |gene|
+  pending # express the regexp above with the code you wish you had
 end
 
 When(/^I enter a guess "(.*?)" for the artwork with id "(.*?)"$/) do |artist, art_id|
   expect(page).to have_content()
 end
 
-Then(/^I should get a point$/) do
-  pending # express the regexp above with the code you wish you had
-end
 
-Then(/^I should not get a point$/) do
+Then(/^I should get "(.*?)" points?$/) do |score|
   pending # express the regexp above with the code you wish you had
 end
