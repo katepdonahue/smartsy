2a8c40bedaa7eb4cf6ef8698d086754085864408
Kate <katepdonahue@gmail.com>
2014-03-30 15:03:19 -0400

    Refactor to move score logic from view to controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -14,6 +14,15 @@ class GameController < ApplicationController
 
   def update
     @score = 0
+    params.each do |art_id, guess|
+      if art_id.to_i > 0
+        plain_guess = I18n.transliterate(guess).downcase
+        plain_answer = I18n.transliterate(Artwork.find(art_id.to_i).artist.name).downcase
+        if plain_guess == plain_answer
+          @score +=1
+        end
+      end
+    end
     @artworks = Artwork.where(:category => params[:gene])
   end
 
