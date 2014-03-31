3c427a138dde887c24947f620b671d62527e7698
Kate <katepdonahue@gmail.com>
2014-03-31 10:58:04 -0400

    Add logic to also show score as percentage

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,18 +4,16 @@ class GameController < ApplicationController
 
   end
 
-  def show
-    @artworks = Artwork.where(:category => params[:gene])
-  end
-
   def edit
     @artworks = Artwork.where(:category => params[:gene])
   end
 
   def update
     @score = 0
+    total = 0
     params.each do |art_id, guess|
       if art_id.to_i > 0
+        total +=1
         plain_guess = I18n.transliterate(guess).downcase
         plain_answer = I18n.transliterate(Artwork.find(art_id.to_i).artist.name).downcase
         if plain_guess == plain_answer
@@ -23,6 +21,7 @@ class GameController < ApplicationController
         end
       end
     end
+    @percent = "#{@score/total * 100}%"
     @artworks = Artwork.where(:category => params[:gene])
   end
 
