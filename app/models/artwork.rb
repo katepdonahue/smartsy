8a1ab36f947e390c834d549dbf11091c9538765a
Kate <katepdonahue@gmail.com>
2014-03-31 10:54:37 -0400

    Get rid of art_obj class check, that was there to attempt to fix the error about there being no image_id for class object but that was because I was calling image_id as a variable instead of a method

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -4,16 +4,14 @@ class Artwork < ActiveRecord::Base
 
   def self.clean_imgs(gene)
     self.where(:category => gene).each do |art_obj|
-      if art_obj.class == Artwork
-        response = HTTParty.get("https://d1ycxz9plii3tb.cloudfront.net/additional_images/#{art_obj.image_id}/tall.jpg")
-        i = 0
-        while response["Error"] && i < 6
-          i += 1
-          response = HTTParty.get("https://d1ycxz9plii3tb.cloudfront.net/additional_images/#{art_obj.image_id}/#{i}/tall.jpg")
-        end
-        art_obj.image_id = "#{art_obj.image_id}/#{i}" if i > 0
-        art_obj.save
+      response = HTTParty.get("https://d1ycxz9plii3tb.cloudfront.net/additional_images/#{art_obj.image_id}/tall.jpg")
+      i = 0
+      while response["Error"] && i < 6
+        i += 1
+        response = HTTParty.get("https://d1ycxz9plii3tb.cloudfront.net/additional_images/#{art_obj.image_id}/#{i}/tall.jpg")
       end
+      art_obj.image_id = "#{art_obj.image_id}/#{i}" if i > 0
+      art_obj.save
     end
   end
 
