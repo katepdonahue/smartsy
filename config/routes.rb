c203da18ddbeb954853dc58a5f0971fbfb1e0df4
Kate <katepdonahue@gmail.com>
2014-03-31 11:18:39 -0400

    Add root route to index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Smartsy::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'game#index'
+
   get 'games/:gene/play' => 'game#edit'
   post 'games/:gene' => 'game#update'
   get 'games/:gene' => 'game#show'
