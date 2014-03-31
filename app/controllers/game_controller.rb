092fa4890be7fdd64d05548951cbfe87629b68e4
Kate <katepdonahue@gmail.com>
2014-03-31 11:08:23 -0400

    Add percentage to score page

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -10,10 +10,10 @@ class GameController < ApplicationController
 
   def update
     @score = 0
-    total = 0
+    @total = 0
     params.each do |art_id, guess|
       if art_id.to_i > 0
-        total +=1
+        @total +=1
         plain_guess = I18n.transliterate(guess).downcase
         plain_answer = I18n.transliterate(Artwork.find(art_id.to_i).artist.name).downcase
         if plain_guess == plain_answer
@@ -21,7 +21,7 @@ class GameController < ApplicationController
         end
       end
     end
-    @percent = "#{@score/total * 100}%"
+    @percent = (@score.to_f / @total.to_f) * 100.0
     @artworks = Artwork.where(:category => params[:gene])
   end
 
